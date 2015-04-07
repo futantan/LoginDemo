c7d8d9197493e27afe8e59557674d11c133dfcde
futantan <luckytantanfu@gmail.com>
2015-04-07 20:59:00 +0800

    新建、添加管理员数据库记录

--- src/user.sql
+++ src/user.sql
@@ -10,4 +10,13 @@ CREATE TABLE user (
   email       VARCHAR(55)       NOT NULL,
   regist_time DATETIME          NOT NULL,
   isConfirmed TINYINT DEFAULT 0 NOT NULL
-)
+);
+
+DROP TABLE IF EXISTS amdin;
+
+CREATE TABLE admin (
+  id       INT PRIMARY KEY UNIQUE AUTO_INCREMENT,
+  name     VARCHAR(50) NOT NULL,
+  password VARCHAR(50) NOT NULL
+);
+INSERT INTO admin (name, password) VALUES ('admin', '21232f297a57a5a743894a0e4a801fc3');
